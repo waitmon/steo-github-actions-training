1b8f6e684c0406eb25068800f5e7247700f25275
antnmow <antnmow2017@yandex.ru>
2024-06-30 19:30:05 +0300

    remove txt files

--- tests/test_button.py
+++ tests/test_button.py
@@ -7,7 +7,7 @@ import pytest
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 
